0c9406285666b12d906e96b01a4c14175aaefdc5
Namuduri Sravani <srava@DESKTOP-0A68488.MyDomain.com>
2021-05-12 18:26:27 -0700

    first commit

---
+++ Assignment01/src/com/trainings/Initials.java
@@ -0,0 +1,15 @@
+package com.trainings;
+
+public class Initials {
+    public static void main(String args[]){
+        System.out.println("**        ***         ***********          **                   *                  **");
+        System.out.println("**      ***           **           *        **                 ***                **");
+        System.out.println("**    ***             **             **       **               ** **             **");
+        System.out.println("**   ***              **             **         **            **   **           **");
+        System.out.println("*****                 **             **           **         **      **        **");
+        System.out.println("**   ***              **             **             **      **        **      **");
+        System.out.println("**       ***          **            **                **   **          **   **");
+        System.out.println("**          ***       **           *                     ***             ***");
+        System.out.println("**              ***   ************                        *               *");
+    }
+}
---
+++ Assignment01/src/com/trainings/Tenhelloworlds.java
@@ -0,0 +1,19 @@
+package com.trainings;
+
+public class Tenhelloworlds {
+    public static void main(String args [])
+    {
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+    }
+}
+
+
